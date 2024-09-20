1f21c9bc59482208ae1e319281e4dbe00f63f251
unknown <akdharaniakdharan@gmail.com>
2024-09-20 12:02:22 +0530

    Updates

---
+++ precache-manifest.05c27578615e7b76654c45aacf5f1ed6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "715731b399f0de3f2a6a",
+    "url": "/javascript-project/static/css/main.72f1682f.chunk.css"
+  },
+  {
+    "revision": "715731b399f0de3f2a6a",
+    "url": "/javascript-project/static/js/main.715731b3.chunk.js"
+  },
+  {
+    "revision": "f68dcc1a0c0c6e129be0",
+    "url": "/javascript-project/static/js/1.f68dcc1a.chunk.js"
+  },
+  {
+    "revision": "0d1b245dcc1d3648082a",
+    "url": "/javascript-project/static/js/runtime~main.0d1b245d.js"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/javascript-project/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/javascript-project/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/javascript-project/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "26a930fa119421bd48590481a5122090",
+    "url": "/javascript-project/index.html"
+  }
+];
